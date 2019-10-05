ddc65e01fc507718a9181a59a497bf2cf56efcc4
lukezc90 <bussi@sina.com>
2019-10-05 14:16:11 +0800

    demo1

---
+++ src/test/java/junit4demo.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class junit4demo {
+    @Test
+    public void testdeom() {
+        assertTrue(true);
+    }
+    @Test
+    public void testdeom2(){
+        assertTrue(false);
+    }
+}
